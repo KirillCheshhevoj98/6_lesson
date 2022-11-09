3b05f26b18f45dbfc6aaf25260fa231b2e9c7e9a
KirillCheshhevoj98 <111186687+KirillCheshhevoj98@users.noreply.github.com>
2022-11-09 18:52:23 +0300

    Add files via upload

--- Task_14.cpp
+++ Task_14.cpp
@@ -7,14 +7,31 @@ int main()
 	*/
 
 	int a, b, c, sum = 0;
-
+	
+	/*
+	for(step = 4; step >= 0; step--)
+	{
+		last = x % 10;
+		res = res + last * pow(10, step);
+		x = x / 10;
+	}
+	printf("%d", res);
+		
+	for(i = 1; i <= 5; i++)
+	{
+		last = x % 10;
+		res = res * 10 + last;
+		x = x / 10;
+	}
+	printf("%d", res);
+	*/
 	printf("Enter number a: ");
 	scanf("%d", &a);
 	if (a > 9999 && a < 100000)
 		for (int i = a; i; i /= 10)
-	{
+		{
 			printf("%d", i % 10);
-	}
+		}
 	getchar();
 	return 0;
 }
